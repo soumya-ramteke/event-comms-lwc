f6b7faefa953d6ce0fbd288b7a7a1b1870aa740d
soumya-ramteke <soumya.r@cgi.com>
2021-01-28 12:02:58 +0530

    public function call

--- force-app/main/default/lwc/augmentor/augmentor.js
+++ force-app/main/default/lwc/augmentor/augmentor.js
@@ -8,4 +8,11 @@ export default class Augmentor extends LightningElement {
     // <c-numerator counter={startCounter}> line #16
     this.startCounter = parseInt(event.target.value);
   }
+
+  handleMaximizeCounter() {
+    // because maximizeCounter is defined in numerator
+    // counter is numerator's property
+    // that function cannot be called from the parent, as it doesnt know actually what value counter is currently
+    this.template.querySelector("c-numerator").maximizeCounter();
+  }
 }
--- force-app/main/default/lwc/numerator/numerator.js
+++ force-app/main/default/lwc/numerator/numerator.js
@@ -2,7 +2,11 @@ import { LightningElement, api } from "lwc";
 
 export default class Numerator extends LightningElement {
   @api counter = 0;
-
+  // parent will call this maximizeCounter
+  @api
+  maximizeCounter() {
+    this.counter += 1000000;
+  } // add a button and handler for handling this in augmentor (parent)
   handleIncrement() {
     this.counter++;
   }
