20b618c4a86f60ca9b7a45ea45b526e6c1b780cb
soumya-ramteke <soumya.r@cgi.com>
2021-01-28 12:32:08 +0530

    getter and setter

--- force-app/main/default/lwc/numerator/numerator.js
+++ force-app/main/default/lwc/numerator/numerator.js
@@ -1,7 +1,21 @@
 import { LightningElement, api } from "lwc";
 
 export default class Numerator extends LightningElement {
-  @api counter = 0;
+  // now making counter accessible through get and set
+  // @api counter = 0;
+
+  _currentCount = 0;
+  priorCount = 0;
+
+  @api
+  get counter() {
+    return this._currentCount;
+  }
+  set counter(value) {
+    this.priorCount = this._currentCount;
+    this._currentCount = value;
+  }
+
   // parent will call this maximizeCounter
   @api
   maximizeCounter() {
